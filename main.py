6c32bbf62112397f386e5e069c74da70d5c77ba1
sriharsha <sriharshadabbiru04@gmail.com>
2025-09-12 16:46:49 +0530

    add mongo setup

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from db import employees_collection
 
 app=FastAPI()
 
 @app.get('/')
 async def root():
     return {"message":"I am Sri Harsha"}
+
+@app.get('/test-db')
+async def test_db():
+    doc=await employees_collection.insert_one({"test":"testing"})
+    return {"inserted_id": str(doc.inserted_id)}
